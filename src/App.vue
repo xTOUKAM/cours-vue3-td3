d8163c5d2a171c8eb90aabb2e57f80a37a0176d6
Tom HOULLEGATTE <houllegatte.tom@gmail.com>
2025-03-05 11:16:06 +0100

    Adding PostCard components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { computed, ref } from "vue";
+import PostCard from "@/components/PostCard.vue";
 
 const text = ref("");
 const trimmedText = computed(() => text.value.trim());
@@ -19,6 +20,10 @@ function addPost() {
   posts.value.push(newPost);
   text.value = "";
 }
+
+function deletePost(id) {
+  posts.value = posts.value.filter((post) => post.id !== id);
+}
 </script>
 
 <template>
@@ -31,94 +36,12 @@ function addPost() {
 
       <p v-if="!posts.length">Pas de post pour le moment.</p>
 
-      <article v-for="post in [...posts].reverse()" :key="post.id" class="card">
-        <div class="post-header">
-          <img
-            :src="post.author.avatarUrl"
-            alt="Avatar"
-            width="40"
-            height="40"
-            class="user-image"
-          />
-          <a>{{ post.author.username }}</a>
-        </div>
-        <p>{{ post.content }}</p>
-      </article>
+      <PostCard
+        v-for="post in [...posts].reverse()"
+        :key="post.id"
+        :post="post"
+        @delete="deletePost"
+      />
     </div>
   </main>
 </template>
-
-<style scoped>
-.container {
-  height: 100vh;
-  margin: 0 auto;
-  max-width: 640px;
-}
-.card {
-  background-color: var(--color-bg-secondary);
-  border-radius: 10px;
-  border: 1px solid var(--color-border);
-  margin-bottom: 1rem;
-}
-
-form {
-  display: flex;
-  flex-direction: column;
-  margin-top: 1rem;
-  padding: 1rem 1.5rem;
-  width: 100%;
-}
-textarea {
-  background: none;
-  border: none;
-  color: var(--color-text-primary);
-  flex: 1;
-  margin-bottom: 1rem;
-  outline: none;
-  padding: 0.5rem 0;
-  resize: none;
-  field-sizing: content;
-}
-button {
-  align-self: flex-end;
-  background: none;
-  border-radius: 10px;
-  border: 1px solid var(--color-border);
-  color: var(--color-text-primary);
-  cursor: pointer;
-  font-size: 1rem;
-  height: 40px;
-  padding: 0 1rem;
-}
-
-button:disabled {
-  cursor: not-allowed;
-  opacity: 0.4;
-}
-
-button:hover {
-  transition: background-color 0.3s;
-  background-color: var(--color-bg-tertiary);
-}
-
-article {
-  padding: 0.75rem 1.5rem;
-  overflow: hidden;
-}
-
-article p {
-  white-space: pre-wrap;
-}
-
-.user-image {
-  border-radius: 50%;
-  object-fit: cover;
-}
-
-.post-header {
-  align-items: center;
-  display: flex;
-  margin-bottom: 1rem;
-  gap: 1rem;
-}
-</style>
---
+++ src/components/PostCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { TrashIcon } from "@heroicons/vue/24/outline";
+defineProps(["post"]);
+const emit = defineEmits(["delete"]);
+</script>
+
+<template>
+  <article class="card">
+    <header>
+      <img :src="post.author.avatarUrl" alt="Avatar" width="36" height="36" class="avatar" />
+      <a>{{ post.author.username }}</a>
+    </header>
+    <p>{{ post.content }}</p>
+
+    <footer>
+      <button class="btn-icon trash-icon" @click="emit('delete', post.id)">
+        <TrashIcon />
+      </button>
+    </footer>
+  </article>
+</template>
